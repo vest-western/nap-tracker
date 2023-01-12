a8e9731b0b6c0bfc370781eb909dee15e9a2bce3
vest-western <99530701+vest-western@users.noreply.github.com>
2023-01-11 22:19:32 -0700

    remove moreboilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App">Hello, world!</div>;
+  return <div>Hello, world!</div>;
 }
 
 export default App;
